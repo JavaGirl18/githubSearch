cbbcd5045cd34a2c5b59ad81fe98ac0172e9e4bb
JavaGirl18 <valenciacooper89@gmail.com>
2020-09-26 21:06:03 -0400

    fecthing data successfully

--- src/components/GetData.js
+++ src/components/GetData.js
@@ -1,18 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Grid } from "semantic-ui-react";
 
-export default function GetData({ selection, userInput }) {
-  useEffect(() => {
-    if (selection === "Users") {
-      axios.get(`/users/:${userInput}`, {
-        firstName: "Finn",
-        lastName: "Williams"
+export default function GetData(selection, userInput) {
+  // const [result, setResult] = useState([]);
+
+  if (selection === "Users" && userInput.length >= 5) {
+    console.log("user");
+    fetch(`https://api.github.com/search/users?q=${userInput}&per_page=5`)
+      .then(res => res.json())
+      .then(data => {
+        const { login } = data;
+        return login;
       });
-    } else {
-      axios.get(`/users/:${userInput}`, {
-        firstName: "Finn",
-        lastName: "Williams"
+  }
+  if (selection === "Repositories" && userInput.length >= 5) {
+    console.log("repo");
+    fetch(
+      `https://api.github.com/search/repositories?q=${userInput}&per_page=5`
+    )
+      .then(res => res.json())
+      .then(data => {
+        return data;
       });
-    }
-  });
+  } else {
+    return "";
+  }
+
+  // return <div>result{result}</div>;
 }
+// if (selection && userInput) {
+//   console.log({ selection, userInput });
+//   const result = GetData(selection, userInput);
+// }
--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,57 +1,80 @@
-import React, { useState } from "react";
-import { Header, Grid, Input, Dropdown, Icon, Image } from "semantic-ui-react";
+import React, { useState, useRef, useEffect } from "react";
+import { Header, Grid, Input, Dropdown, Icon, Form } from "semantic-ui-react";
+import { GetData } from ".";
 
 export default function MainLayout() {
-  const [selection, setSelection] = useState("Users");
+  const [selection, setSelection] = useState("");
   const [userInput, setUserInput] = useState("");
 
+  const hold = useRef(selection);
   const options = [
     { key: 1, text: "Users", value: "Users" },
     { key: 2, text: "Repositories", value: "Repositories" }
   ];
 
+  const handleSubmit = () => {
+    let r = GetData(selection, userInput);
+
+    console.log(r);
+
+    console.log(login);
+  };
   const handleSelection = (e, { value }) => {
     setSelection(value);
-    if (value.length >= 4) {
-      GetData(value);
-    } else {
-      alert("Must type at least four characters");
-    }
   };
   const handleUserInput = (e, { value }) => {
     setUserInput(value);
   };
   console.log({ selection, userInput });
+
   return (
-    <Grid verticalAlign="top" stackable>
-      <Grid.Row textAlign="left">
-        <Grid.Column width={16} textAlign="left" verticalAlign="middle">
-          <Header as="h4">
-            <Icon name="github" />
-            <Header.Content>GitHubin</Header.Content>
-          </Header>
+    <>
+      {/* <Grid verticalAlign="top" stackable>
+        <Grid.Row textAlign="left">
         </Grid.Column>
-        <Grid.Column width={8}>
-          <Input
-            focus
-            placeholder="Search..."
-            size="big"
-            style={{ width: "325px" }}
-            onChange={handleUserInput}
-          />
-        </Grid.Column>
-        <Grid.Column width={8}>
-          <Dropdown
-            clearable
-            options={options}
-            selection
-            labeled
-            text="Users"
-            onChange={handleSelection}
-            style={{ minHeight: "3.3rem" }}
-          />
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
+        </Grid.Row>
+      </Grid> */}
+      {/* <Grid.Column width={8}> */}
+      <Grid verticalAlign="top" stackable>
+        <Grid.Row>
+          <Grid.Column width={16} textAlign="left" verticalAlign="middle">
+            <Form>
+              <Header as="h4">
+                <Icon name="github" />
+                <Header.Content>GitHubin</Header.Content>
+              </Header>
+              <Form.Group>
+                <Form.Input
+                  focus
+                  placeholder="Search..."
+                  size="big"
+                  style={{ width: "325px" }}
+                  onChange={handleUserInput}
+                />
+                {/* </Grid.Column> */}
+                {/* <Grid.Column width={8}> */}
+                <Form.Select
+                  // clearable
+                  options={options}
+                  selection
+                  onChange={handleSelection}
+                  style={{ minHeight: "3.3rem" }}
+                />
+                <Form.Button
+                  onClick={handleSubmit}
+                  style={{ minHeight: "3.3rem" }}
+                >
+                  Submit
+                </Form.Button>
+              </Form.Group>
+            </Form>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+      {/* </Grid.Column> */}
+      {/* </Grid.Row> */}
+
+      {/* </Grid> */}
+    </>
   );
 }
